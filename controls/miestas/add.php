208b696fc1c5d9e58f49d577553e9a84dc266954
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-05-23 00:33:03 +0300

    Removed unused var dumps. Fixed date validation

--- controls/miestas/add.php
+++ controls/miestas/add.php
@@ -8,12 +8,9 @@ if (!empty($_POST['submit'])) {
     if ($nameErr) {
         $city = $_POST;
         $city['pavadinimas'] = mysql::escape($city['pavadinimas']);
-//        var_dump($city);
         include 'services/miestas.php';
 
         $result = miestas::insertCity($city);
-//        var_dump($result);
-//        die();
         header("Location: index.php?module=miestas&action=list");
     } else {
         $data = $_POST;
