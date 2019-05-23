09b3442609a93934ce2c3a56c443cbfa010dd643
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-05-23 12:34:40 +0300

    Added filtering by order status for report making

--- controls/uzsakymas/report.php
+++ controls/uzsakymas/report.php
@@ -13,10 +13,12 @@ if (!empty($_POST['submit'])) {
     $untilDateErr = $validator->validate($_POST['iki'], "date", 20);
     $toysCountFromErr = $validator->validate($_POST['kiekis_nuo'], "positivenumber");
     $toysCountUntilErr = $validator->validate($_POST['kiekis_iki'], "positivenumber");
+    $statusErr = $validator->validate($_POST['status'], "positivenumber");
 
-    if ($sinceDateErr && $untilDateErr && $toysCountFromErr && $toysCountUntilErr) {
-        $data = $uzsakymuService->selectForReport($_POST['nuo'], $_POST['iki'], $_POST['kiekis_nuo'], $_POST['kiekis_iki']);
-        $limits = [$_POST['nuo'], $_POST['iki'], $_POST['kiekis_nuo'], $_POST['kiekis_iki']];
+    if ($sinceDateErr && $untilDateErr && $toysCountFromErr && $toysCountUntilErr && $statusErr) {
+        $data = $uzsakymuService->getForReport($_POST['nuo'], $_POST['iki'], $_POST['kiekis_nuo'], $_POST['kiekis_iki'], $_POST['status']);
+
+        $limits = [$_POST['nuo'], $_POST['iki'], $_POST['kiekis_nuo'], $_POST['kiekis_iki'], $_POST['status']];
     } else {
         $_GET['error'] = 1;
     }
